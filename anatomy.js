6b707198df15a7ce56421b10adf9687718ffd247
Natan Falconi <natan@rgsystem.com.br>
2022-04-06 10:38:31 -0300

    indentação

--- anatomy.js
+++ anatomy.js
@@ -1,31 +1,25 @@
 //FUNÇÃO QUE NÃO RECEBE PARAMETRO E NÃO RETORNA NADA
-function sayHello(){
-    console.log('hello');
+function sayHello() {
+  console.log("hello");
 }
-
 sayHello();
 
-
 //FUNÇÃO QUE RECEBE UM PARAMETRO E NÃO RETORNA NADA
-function sayHelloTo(name){
-    console.log(`Hello ${name}`)
+function sayHelloTo(name) {
+  console.log(`Hello ${name}`);
 }
-
-sayHelloTo('John')
+sayHelloTo("John");
 
 //FUNÇÃO QUE NÃO RECEBE PARAMETRO E RETORNA ALGUMA COISA
 function returnHi() {
-    return 'Hi'
+  return "Hi";
 }
-
-const greeting = returnHi()
-console.log(greeting)
-console.log(returnHi())
-
+const greeting = returnHi();
+console.log(greeting);
+console.log(returnHi());
 
 //FUNÇÃO QUE RECEBE UM PARAMETRO E RETORNA ALGUMA COISA
-function returnHiTo(name){
-    return `Hi ${name}`
+function returnHiTo(name) {
+  return `Hi ${name}`;
 }
-
-console.log(returnHiTo('lucas'));
+console.log(returnHiTo("lucas"));
