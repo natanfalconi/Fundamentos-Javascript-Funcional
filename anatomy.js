e2914952fadc6d6aaf6aba04056806402528d1e1
Natan Falconi <natan@rgsystem.com.br>
2022-04-06 10:37:44 -0300

    comentários

--- anatomy.js
+++ anatomy.js
@@ -1,15 +1,19 @@
+//FUNÇÃO QUE NÃO RECEBE PARAMETRO E NÃO RETORNA NADA
 function sayHello(){
     console.log('hello');
 }
 
 sayHello();
 
+
+//FUNÇÃO QUE RECEBE UM PARAMETRO E NÃO RETORNA NADA
 function sayHelloTo(name){
     console.log(`Hello ${name}`)
 }
 
 sayHelloTo('John')
 
+//FUNÇÃO QUE NÃO RECEBE PARAMETRO E RETORNA ALGUMA COISA
 function returnHi() {
     return 'Hi'
 }
@@ -18,6 +22,8 @@ const greeting = returnHi()
 console.log(greeting)
 console.log(returnHi())
 
+
+//FUNÇÃO QUE RECEBE UM PARAMETRO E RETORNA ALGUMA COISA
 function returnHiTo(name){
     return `Hi ${name}`
 }
