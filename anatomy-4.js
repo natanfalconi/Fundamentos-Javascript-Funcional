2189a0014fd12edce7ed92cefbc148f866e4fef2
falconi <nfalconi65@gmail.com>
2022-04-13 20:54:59 -0300

    IIFE

--- anatomy-4.js
+++ anatomy-4.js
@@ -3,5 +3,18 @@
 // QUANDO USAR? - QUANDO VOCÊ PRECISA GARANTIR QUE DETERMINADOS VALORES
 //ESTARÃO DENTRO DE UM ESCOPO MAIS RESTRITO
 (function (a, b, c) {
-  console.log(`Result ${a + b + c}`);
+  let x = 3;
+  console.log(`Result: ${a + b + c}`);
+  console.log(x);
 })(2, 5, 9);
+
+(function () {
+  let x = 300;
+  console.log(x);
+})();
+
+(() => {
+  console.log("arrow #01");
+})();
+
+(() => console.log("arrow #02"))();
